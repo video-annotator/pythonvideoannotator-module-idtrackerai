e9b0571cbff4f8e756352798a84e00c9e5706d9a
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-05-31 16:56:34 +0100

    Add drag position

--- pythonvideoannotator_module_idtrackerai/models/video/objects/idtrackerai_object.py
+++ pythonvideoannotator_module_idtrackerai/models/video/objects/idtrackerai_object.py
@@ -1,23 +1,29 @@
+import time
+from datetime import datetime
+import copy
 from pythonvideoannotator_models.models.video.objects.video_object import VideoObject
+from pythonvideoannotator_models_gui.models.video.objects.object2d.datasets.dataset_gui import DatasetGUI
+
 from idtrackerai.postprocessing.assign_them_all import close_trajectories_gaps
 from idtrackerai.utils.py_utils import get_spaced_colors_util
-from pythonvideoannotator_models_gui.models.imodel_gui import IModelGUI
 from pyforms.controls import ControlText
 from pyforms.controls import ControlButton
 from pyforms.basewidget import BaseWidget
 from AnyQt import QtCore
 from confapp import conf
 import numpy as np, cv2, math, os
+from idtrackerai.postprocessing.get_trajectories import produce_output_dict
+from idtrackerai.postprocessing.identify_non_assigned_with_interpolation import assign_zeros_with_interpolation_identities
 
 
-class IdtrackeraiObject(IModelGUI, VideoObject, BaseWidget):
+class IdtrackeraiObject(DatasetGUI, VideoObject, BaseWidget):
 
     def __init__(self, video):
 
         self._nametxt = ControlText('Name', default='Untitled')
         self._closepaths_btn = ControlButton('Close trajectories', default=self.__close_trajectories_gaps)
 
-        IModelGUI.__init__(self)
+        DatasetGUI.__init__(self)
         VideoObject.__init__(self)
         BaseWidget.__init__(self, 'IdtrackerAi Object', parent_win=video)
 
@@ -29,15 +35,52 @@ class IdtrackeraiObject(IModelGUI, VideoObject, BaseWidget):
         self.list_of_blobs    = None # IdtrackerAI ListOfBlobs
         self.list_of_framents = None # IdtrackerAI ListOfFragments
 
-        self._selected_id = None
+
+        self._tmp_object_pos = None # used to store the temporary object position on drag
+        self._selected_id = None # id of the selected blob
+        self._selected_pos = None # Position of the selected blob
+        self._selected_blob = None # Selected blob object
 
         self.formset = ['_nametxt', '_closepaths_btn']
 
 
     def save(self, data={}, project_path=None):
         self.list_of_blobs.disconnect()
+        self.list_of_blobs_no_gaps = copy.deepcopy(self.list_of_blobs)
+
         path = os.path.join(project_path, 'preprocessing', 'blobs_collection_no_gaps.npy')
         np.save(path, self.list_of_blobs)
+
+        timestamp_str = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H%M%S')
+
+        trajectories_wo_gaps_file = os.path.join(
+            project_path,
+            'trajectories_wo_gaps',
+            'trajectories_wo_gaps_{}.npy'.format(timestamp_str)
+        )
+
+        trajectories_wo_gaps = produce_output_dict(
+            self.list_of_blobs_no_gaps.blobs_in_video,
+            self.video_object
+        )
+        np.save(trajectories_wo_gaps_file, trajectories_wo_gaps)
+
+        self.list_of_blobs = assign_zeros_with_interpolation_identities(
+            self.list_of_blobs,
+            self.list_of_blobs_no_gaps
+        )
+
+        trajectories_file = os.path.join(
+            self.video_object.trajectories_folder,
+            'trajectories_{}.npy'.format(timestamp_str)
+        )
+        trajectories = produce_output_dict(
+            self.list_of_blobs.blobs_in_video,
+            self.video_object
+        )
+        np.save(trajectories_file, trajectories)
+        self.video_object.save()
+
         return {}
 
     def load_from_idtrackerai(self, project_path, video_object):
@@ -114,9 +157,25 @@ class IdtrackeraiObject(IModelGUI, VideoObject, BaseWidget):
                     cv2.rectangle(frame, bounding_box[0], bounding_box[1], rect_color, 2)
 
 
+        if self._tmp_object_pos:
+            cv2.circle(frame, self._tmp_object_pos, 8, (50,50,50), 2, lineType=cv2.LINE_AA)
+
+    def on_drag(self, p1, p2):
+
+        if self._selected_id and math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)>5:
+            self._tmp_object_pos = int(round(p2[0],0)), int(round(p2[1],0))
+
+
+    def on_end_drag(self, p1, p2):
+        if self._tmp_object_pos:
+            self._selected_blob.centroid = p2
+
+        self._tmp_object_pos = None
+
 
 
     def on_click(self, event, x, y):
+        selected = False
 
         p0          = x, y
         frame_index = self.mainwindow.timeline.value
@@ -136,45 +195,60 @@ class IdtrackeraiObject(IModelGUI, VideoObject, BaseWidget):
 
                     self.mainwindow.player.stop()
 
-                    self._selected_id = identity
-
-                    # ask the new blob identity
-                    new_blob_identity = self.input_int(
-                        'Type in the new identity',
-                        title='New identity',
-                        default=identity if identity is not None else 0
-                    )
-
-                    # Update only if the new identity is different from the old one.
-                    if new_blob_identity!=identity:
-
-                        blob._user_generated_identity = new_blob_identity
-
-                        modified_blob = blob
-                        # to take into account the modification already done in the current frame
-                        count_past_corrections   = 1
-                        count_future_corrections = 0
-                        new_blob_identity        = modified_blob.user_generated_identity
-
-                        if modified_blob.is_an_individual:
-                            current = modified_blob
-
-                            while len(current.next) == 1 and \
-                                current.next[0].fragment_identifier == modified_blob.fragment_identifier:
-
-                                current.next[0]._user_generated_identity = new_blob_identity
-                                current = current.next[0]
-                                count_future_corrections += 1
-
-                            current = modified_blob
-                            while len(current.previous) == 1 and \
-                                current.previous[0].fragment_identifier == modified_blob.fragment_identifier:
-                                current.previous[0]._user_generated_identity = new_blob_identity
-                                current = current.previous[0]
-                                count_past_corrections += 1
-
-                    self.mainwindow.player.refresh()
+                    self._selected_id  = identity
+                    self._selected_pos = p1
+                    self._selected_blob = blob
+                    selected = True
                     break
+
+        if not selected:
+            self._selected_id  = None
+            self._selected_pos = None
+            self._selected_blob = None
+
+
+    def on_double_click(self, event, x, y):
+
+        identity = self._selected_id
+        blob     = self._selected_blob
+
+        if identity is None:
+            return
+
+        # ask the new blob identity
+        new_blob_identity = self.input_int(
+            'Type in the new identity',
+            title='New identity',
+            default=identity if identity is not None else 0
+        )
+
+        # Update only if the new identity is different from the old one.
+        if new_blob_identity!=identity:
+
+            blob._user_generated_identity = new_blob_identity
+
+            modified_blob = blob
+            # to take into account the modification already done in the current frame
+            count_past_corrections   = 1
+            count_future_corrections = 0
+            new_blob_identity        = modified_blob.user_generated_identity
+
+            if modified_blob.is_an_individual:
+                current = modified_blob
+
+                while len(current.next) == 1 and \
+                    current.next[0].fragment_identifier == modified_blob.fragment_identifier:
+
+                    current.next[0]._user_generated_identity = new_blob_identity
+                    current = current.next[0]
+                    count_future_corrections += 1
+
+                current = modified_blob
+                while len(current.previous) == 1 and \
+                    current.previous[0].fragment_identifier == modified_blob.fragment_identifier:
+                    current.previous[0]._user_generated_identity = new_blob_identity
+                    current = current.previous[0]
+                    count_past_corrections += 1
 
 
 
