1602d7ec8cf9fa4d991666d88daf1eaf69babfd7
Francisco Romero <francisco.romero@neuro.fchampalimaud.org>
2021-11-09 10:56:34 +0100

    update version

--- pythonvideoannotator_module_idtrackerai/__init__.py
+++ pythonvideoannotator_module_idtrackerai/__init__.py
@@ -3,9 +3,9 @@ from confapp import conf
 
 logger = logging.getLogger(__name__)
 logger.setLevel(conf.APP_LOG_HANDLER_LEVEL)
-logging.getLogger('idtrackerai').setLevel(logging.DEBUG)
+logging.getLogger("idtrackerai").setLevel(logging.DEBUG)
 
 from .settings import IDTRACKERAI_SHORT_KEYS
 
 conf.SHORT_KEYS.update(IDTRACKERAI_SHORT_KEYS)
-__version__ = "0.6.67"
+__version__ = "1.0.0a0"
